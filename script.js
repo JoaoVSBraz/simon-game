5b798f355e519473c01ac9b69c5cc4a0565c913b
JoaoVSBraz <joaovictor3.14@outlook.com>
2022-01-12 15:47:01 -0300

    :construction: Add boolean var& keydown event listener to start

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ var buttonColours = ['red', 'blue', 'green', 'yellow']
 
 var gamePattern = []
 var userClickedPattern = []
+var started = false
+var level = 0
+
+$(document).keydown(function () {
+    if (!started) {
+        $('#level-title').text('Level ' + level)
+        nextSequence()
+        started = true
+    }
+})
 
 $('.btn').click(function () {
 
@@ -14,6 +24,10 @@ $('.btn').click(function () {
 })
 
 function nextSequence() {
+
+    level++
+
+    $('#level-title').text('Level ' + level)
 
     var randomNumber = Math.floor(Math.random() * 4)
     var randomChosenColour = buttonColours[randomNumber]
